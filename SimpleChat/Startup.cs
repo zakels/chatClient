e54137586cfdaf28588fa9990569dc1bfbb5d3c5
zakels <kj.j.cha@gmail.com>
2019-04-03 17:05:31 +0900

    초기 커밋.

---
+++ SimpleChat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleChat.Startup))]
+namespace SimpleChat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
